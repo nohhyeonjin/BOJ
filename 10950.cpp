40580a2d16690fdbf8a2fb287c71eb92778fafcf
Noh Hyeon Jin <hjforj@naver.com>
2019-08-06 00:31:03 +0900

    day1

---
+++ 10950.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t,a,b;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        scanf("%d %d",&a,&b);
+        printf("%d\n",a+b);
+    }
+}
