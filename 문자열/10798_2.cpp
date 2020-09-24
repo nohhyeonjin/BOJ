f70cad3e1dd71d87b46bb01ca1fa496f7160e198
Noh Hyeon Jin <hjforj@naver.com>
2020-09-24 19:54:56 +0900

    200924

---
+++ 문자열/10798_2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s[5];
+    for(int i=0;i<5;i++) cin >> s[i];
+    for(int i=0;i<15;i++){
+        for(int j=0;j<5;j++){
+            if(s[j].size()>i) printf("%c",s[j][i]);
+        }
+    }
+}
