3320007285bd7b6d39ac3f5fd2e440f1944cb402
Noh Hyeon Jin <hjforj@naver.com>
2020-11-03 18:25:05 +0900

    201103

---
+++ 그래프/9372.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int n,m;
+        scanf("%d %d",&n,&m);
+        for(int i=0;i<m;i++){
+            int a,b;
+            scanf("%d %d",&a,&b);
+        }
+        printf("%d\n",n-1);
+    }
+}
