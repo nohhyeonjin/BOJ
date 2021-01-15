70c62cdc4221c24a26036fefdfb49eaef868ce6b
Noh Hyeon Jin <hjforj@naver.com>
2021-01-16 04:07:58 +0900

    210116

--- 수학/2745.cpp
+++ 수학/2745.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    string n;
+    int b;
+    cin >> n >> b;
+    int ans=0;
+    int mul=0;
+    for(int i=n.size()-1;i>=0;i--){
+        int num;
+        if(isalpha(n[i])) num=n[i]-'A'+10;
+        else num=n[i]-'0';
+        ans+=(num*pow(b,mul++));
+    }
+    cout << ans;
 }
