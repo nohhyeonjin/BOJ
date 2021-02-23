d99f641074ba9546d021db23d9660e8f976bfa2c
Noh Hyeon Jin <hjforj@naver.com>
2021-02-23 22:52:49 +0900

    210223

--- 완전탐색/1182_2.cpp
+++ 완전탐색/1182_2.cpp
@@ -1,6 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
+int n,s;
+int num[20];
+int ans;
+int result[20];
+bool selected[20];
+void dfs(int idx, int depth, int cnt){
+    if(depth==cnt){
+        int sum=0;
+        for(int i=0;i<cnt;i++) sum+=result[i];
+        if(sum==s) ans++;
+        return;
+    }
+    for(int i=idx;i<n;i++){
+        if(selected[i]) continue;
+        selected[i]=true;
+        result[depth]=num[i];
+        dfs(i+1,depth+1,cnt);
+        selected[i]=false;
+    }
+}
 int main(){
-    scanf("%d",&n);
+    scanf("%d %d",&n,&s);
+    for(int i=0;i<n;i++) scanf("%d",&num[i]);
+    for(int i=1;i<=n;i++) dfs(0,0,i);
+    printf("%d",ans);
 }
